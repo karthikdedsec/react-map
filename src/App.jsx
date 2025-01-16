1093cc7932f13b62c6847cdfc95979d1fb683083
Karthik Raj <karthikkap121@gmail.com>
2025-01-16 16:51:52 +0400

    clip path

--- src/App.jsx
+++ src/App.jsx
@@ -1,16 +1,16 @@
 import "./App.css";
 import AbuDhabiMap from "./components/AbuDhabiMap";
+import AbuImg from "./assets/abudhabi-map.png";
+import AbuMap from "./assets/AEAZ.png";
 
 function App() {
   return (
     <>
       <section className="map-container">
-        <img
-          src="https://images.unsplash.com/photo-1603349995009-369a029c14a5?q=80&w=1786&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D"
-          alt=""
-          className="stock-image"
-        />
-        <AbuDhabiMap />
+        <div className="abu-container">
+          <AbuDhabiMap />
+        </div>
+        <img src={AbuMap} alt="" className="stock-image" />
       </section>
     </>
   );
